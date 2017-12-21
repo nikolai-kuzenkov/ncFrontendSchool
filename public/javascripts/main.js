d14d0fcb594c6e4bd0ca4c8917d1f85d214f5ba0
Ilya Isupov <ilis1014@netcracker.com>
2017-12-21 17:12:05 +0300

    Add client routing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,97 @@
 (function($) {
 
+    var Validation = {
+        init: function() {
+
+        },
+        validate: function(data) {
+            return true;
+        }
+    };
+
+    var AppView = {
+        init: function() {
+            this.content = $("#main").find("#content");
+        },
+        redraw: function(redrawData) {
+            var data = redrawData.data;
+            var successCallback = redrawData.successCallback;
+            var _this = this;
+            this.content.fadeOut(function() {
+                _this.content.html(data);
+                _this.content.fadeIn(function() {
+                    if(successCallback != undefined) {
+                        successCallback(data);
+                    }
+                });
+            });
+        }
+    };
+
     var Navigation = {
         init: function() {
             this.createHandlers();
+            this.checkURL();
+        },
+        checkURL: function() {
+            var resource = document.location.pathname;
+            this.navigationStep.processRequest(resource, Navigation.wizard.getPageByResource(resource).redrawCallback);
         },
         createHandlers: function() {
             this.mainElement = $("#main");
             this.navigation = this.mainElement.find(".nav");
-            this.createTicketToService = this.navigation.find("#createTicketToService");
-            this.createTicketToService.click(this.handleNavClick.process);
+            this.navElements = this.navigation.find(".navigationButton");
+            this.navElements.click(this.navigationStep.handleClick);
         },
-        handleNavClick: {
-            process: function(event) {
+        navigationStep: {
+            handleClick: function(event) {
+                var resource = $(this).data("loadresourse");
+                Navigation.navigationStep.processRequest(resource, Navigation.wizard.getPageByResource(resource).redrawCallback);
+            },
+            processRequest: function(resource, successCallback) {
                 $.ajax({
-                    url: "/blocks/clientForm.html",
+                    url: resource,
+                    type: "post",
                     dataType: "html",
                     success: function(data) {
-                        var content = $("#content");
-                        content.html(data);
+                        AppView.redraw({data: data, successCallback: successCallback});
+                    },
+                    error: function(xhr) {
+                        alert("Not OK");
+                    }
+                });
+            }
+        },
+        wizard: {
+            getPageByResource: function(resource) {
+                var page;
+                var pages = $.map(Navigation.wizard.pages, function(value, index) {
+                    return [value];
+                });
+                pages.forEach(function(item, i) {
+                    if(item.resource == resource)
+                        page = item;
+                });
+                return page;
+            },
+            pages: {
+                main: {
+                    name: "Main",
+                    resource: "/",
+                    redrawCallback: function(data) {
+
+                    }
+                },
+                createTicket: {
+                    name: "Create Ticket",
+                    resource: "/createTicket",
+                    redrawCallback: function(data) {
                         var clientForm = $("#clientForm");
-                        content.fadeIn();
                         clientForm.find("input[type='submit']").click(function(event) {
                             event.preventDefault();
                             var formData = {};
                             clientForm.serializeArray().map(function(x){formData[x.name] = x.value;});
-                            var go = clientValidate(formData);
-                            if(go) {
+                            if(Validation.validate(formData)) {
                                 $.ajax({
                                     url: "/getResource",
                                     dataType: "json",
@@ -41,8 +107,7 @@
                                         var status = $("<div>");
                                         status.text(responseData.status);
                                         div.append(header).append(datefield).append(status);
-                                        content.empty();
-                                        content.append(div);
+                                        $("#content").append(div);
                                     },
                                     error: function(xhr) {
                                         console.log(xhr.responseText);
@@ -51,44 +116,17 @@
                             }
 
                         });
-                    },
-                    error: function() {
-                        alert("Not OK");
                     }
-                });
+                }
             }
         }
-
-    };
-
-    var State = {
-        init: function() {
-            this.getResource();
-        },
-        getResource: function() {
-            $.ajax({
-                url: document.location.pathname,
-                dataType: "html",
-                success: function(data) {
-
-
-                },
-                error: function() {
-                    alert("Not OK");
-                }
-            });
-        }
     };
 
 
-    $(document).ready(function(){
-        State.init();
+    $(document).ready(function() {
+        AppView.init();
         Navigation.init();
     });
-
-
-
-
 
 })(jQuery);
 

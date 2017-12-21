c20fead37253d460d356f8be94eb9b71467f22d4
Ilya Isupov <ilis1014@netcracker.com>
2017-12-21 17:09:55 +0300

    Add client routing

--- app.js
+++ app.js
@@ -11,19 +11,29 @@ app.post('/getResource', function (req, res) {
         date: "19:00MSK 13.12.2017"
     });
 });
-app.get('/getResource', function (req, res) {
-    console.log("Your GET request:" + req.body.toString());
-    res.setHeader('Content-Type', 'application/json');
-    res.send({name: "Ilya"});
+
+
+app.get('/createTicket', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+app.post('/createTicket', function (req, res) {
+    res.sendfile(__dirname + '/public/blocks/clientForm.html');
 });
 
-app.get('/contacts', function (req, res) {
+
+
+app.post('/contacts', function (req, res) {
     console.log("Your GET request:" + req.body.toString());
     res.setHeader('Content-Type', 'application/json');
     res.send({location: "contacts.html", pageName: "Contacts"});
 });
+
 app.get('/', function(req,res){
     res.sendfile(__dirname + '/public/index.html');
+});
+
+app.post('/', function(req,res){
+    res.sendfile(__dirname + '/public/blocks/navigation.html');
 });
 
 app.get('/blocks/clientForm.html', function(req,res){
@@ -33,12 +43,8 @@ app.get('/javascripts/main.js', function(req,res){
     res.sendfile(__dirname + '/public/javascripts/main.js');
 });
 
-app.get('/contacts', function(req,res){
-    res.send({url: __dirname + '/public/index.html', state: "Contacts"});
-});
-
-app.get('/stylesheets/main.css', function(req,res){
-    res.sendfile(__dirname + '/public/stylesheets/main.css');
+app.get('/stylesheets/main.less', function(req,res){
+    res.sendfile(__dirname + '/public/stylesheets/main.less');
 });
 app.listen(3000, function () {
     console.log('Listening on port 3000...');
